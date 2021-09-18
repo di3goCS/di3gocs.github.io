6706dfbcc6f1b8c9baf87e22c71b77d3e2e98bf2
di3goCS <diego.cs282@gmail.com>
2021-09-18 20:52:46 -0300

    fix again

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:di3goCS/di3gocs.github.io.git master:gh-pages
+git push -f git@github.com:di3goCS/di3gocs.github.io.git main
 cd -
