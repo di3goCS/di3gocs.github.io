9a82ab8ce26bb58eabc818e056d2f4f1934f696b
di3goCS <diego.cs282@gmail.com>
2021-09-18 21:14:19 -0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,7 @@ npm run build
 cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
-git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:di3goCS/di3gocs.github.io.git main
+git push origin main
 cd -
