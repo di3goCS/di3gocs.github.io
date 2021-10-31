853bbb9a8396dd411965392f8cd4184b78c9d5dc
di3goCS <diego.cs282@gmail.com>
2021-10-31 11:25:21 -0300

    change title

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
         config
             .plugin('html')
             .tap(args => {
-                args[0].title = "Di3goCS - Desenvolvedor Web";
+                args[0].title = "Di3goCS - Desenvolvedor de Software";
                 return args;
             })
     },
